5fa22281f3d98698d4045d452196a9ce9f098f73
Melroy Barreto <melroybarreto61@gmail.com>
2025-08-28 22:52:02 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser"
+
+const port = 3000;
+const router = express();
+
+router.use(express.static("public"));
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+router.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
