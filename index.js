aceb7af6783b40874dee1c6b80832678beb7b01c
Melroy Barreto <melroybarreto61@gmail.com>
2025-10-21 21:04:14 +0530

    bootstrap package import

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const port = 3000;
 const router = express();
 
-router.use(express.static("public"));
+// console.log(__filename);
+// console.log(__dirname);
+
+//serve static files (css, js)
+router.use(express.static(path.join(__dirname,'public')));
+
+//serve bootstrap from node modules
+router.use('/bootstrap',express.static(path.join(__dirname, 'node-modules/bootstrap/dist')));    
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/", (req, res) => {
