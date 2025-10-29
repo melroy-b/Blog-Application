13988711ae559a6cb36333e0c3edb3b78d7b9e8b
Melroy Barreto <melroybarreto61@gmail.com>
2025-10-29 23:00:21 +0530

    created post request routes for blog entry

--- index.js
+++ index.js
@@ -2,27 +2,62 @@ import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser"
 import path, { dirname } from "path"
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";    
+import axios from "axios";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = 3000;
-const router = express();
+const app = express();
 
+let blogPosts = [];
+let idCounter = 0;
 // console.log(__filename);
 // console.log(__dirname);
 
 //serve static files (css, js)
-router.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 //serve bootstrap from node modules
-router.use('/bootstrap',express.static(path.join(__dirname, 'node-modules/bootstrap/dist')));    
-router.use(bodyParser.urlencoded({ extended: true }));
+app.use('/bootstrap',express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));    
+app.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+    res.render("index.ejs", {blogPosts});
 })
 
-router.listen(port, () => {
+app.get("/about", (req, res) => {
+    console.log("About route accessed");
+    res.render("about.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+    try {
+        console.log("Contact route accessed");
+    } catch (error) {   
+        console.error("Error rendering contact page:", error);
+        res.status(500).send("Internal Server Error");
+    }
+
+})
+
+app.get("/create-post", (req, res) => {
+    console.log("Create Post route accessed");
+    res.render("create-post.ejs");
+})
+
+app.post("/create-post", (req, res) => {
+    console.log("Form Data:", req.body.title, req.body.content, req.body.author);
+    const { title, content, author } = req.body;
+    const id = idCounter++;
+    blogPosts.push({ title, content, author, id });
+    console.log("Blog post created with ID:", blogPosts);
+
+    res.redirect("/");
+})
+
+
+app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 })
