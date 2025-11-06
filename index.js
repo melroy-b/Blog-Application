32dd08a82d687c13da875be8ecf1cead0a5e5107
Melroy Barreto <64680497+melroy-b@users.noreply.github.com>
2025-11-06 13:45:36 +0530

    Use environment variable for port configuration
    For Railway deployment fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import methodOverride from "method-override";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 let blogPosts = [];
